5a608266d273bd4d7afaf97751b8254c6a3811ec
ectuser <m.ectuser@gmail.com>
2022-03-19 11:25:39 +0100

    Added ngrx

--- apps/vg-rmod-fe-app/src/app/app.module.ts
+++ apps/vg-rmod-fe-app/src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { LayoutModule } from './core/layout/layout.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SharedModule } from './shared/shared.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule, NgbModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, NgbModule, SharedModule, StoreModule.forRoot({}, {}), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }), EffectsModule.forRoot([])],
   providers: [],
   bootstrap: [AppComponent],
 })
